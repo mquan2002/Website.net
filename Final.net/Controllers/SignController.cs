9348acb97cca7bc54049b850adb23dfa0e28f65d
Joe Sixpack <joe.sixpack@g_mail.com>
2024-11-12 03:01:50 +0700

    Chỉnh sửa trang Home; thêm trang Menu; thêm trang SignIn, SignUp; tạo điều hướng cho các button chuyển trang

---
+++ Final.net/Controllers/SignController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Final.net.Controllers
+{
+    public class SignController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
