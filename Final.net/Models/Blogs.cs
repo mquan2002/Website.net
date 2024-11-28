9c1fadb9ca6127787b26ec4d5150bb8dfc48f919
mquan2002 <mquan532002@gmail.com>
2024-11-29 04:02:36 +0700

    Update css các trang quản lý admin, add thêm blog và quản lý blog

---
+++ Final.net/Models/Blogs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Final.net.Models
+{
+    [Table("Blogs")] 
+    public class Blogs
+    {
+        [Key] 
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+        public int Id { get; set; }
+
+        [Required] 
+        [StringLength(255)] 
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(500)] 
+        public string ImageURL { get; set; }
+
+        [Required]
+        [StringLength(500)] 
+        public string LinkURL { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now; 
+        public DateTime? UpdatedAt { get; set; } 
+
+        [Required]
+        public bool Active { get; set; }
+
+        public void UpdateTimestamp()
+        {
+            this.UpdatedAt = DateTime.Now;
+        }
+    }
+}
